ae1c22ca17c9bcb342fcd4ff8819eb05177cf1fe
Antônio Lucas C Araújo <156518068+ALucas314@users.noreply.github.com>
2024-09-29 20:53:20 -0300

    Update Business.jsx

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -38,7 +38,7 @@ const Business = () => (
         no prazo.
       </p>
 
-      <a href="http://127.0.0.1:5500/index.html">
+      <a href="https://chat-bot-alpha-coral.vercel.app/">
         <Button styles={`mt-10`} />
       </a>
     </div>
